922fa0506caf3c5ba68c9dc3f6d3714689fd4933
sudhirr@karunya.edu.in <sudhirr@karunya.edu.in>
2024-10-03 22:30:27 +0530

    added delete button (wo functionality)

--- to-do-app/src/components/Items.jsx
+++ to-do-app/src/components/Items.jsx
@@ -3,8 +3,14 @@ export default function Items({item}) {
     return (
         <div>
             <div className={styles.It_comp}>
-            <h3 className={styles.It_style}>{item}</h3>
-            <hr/>
+            <h3 className={styles.It_style}>{item}
+            <span>
+                <button className={styles.deleteButton}>Delete</button>
+            </span>
+            </h3>
+            
+            <hr className={styles.line}/>
+            
             </div>
         </div>
         
--- to-do-app/src/components/ToDoList.jsx
+++ to-do-app/src/components/ToDoList.jsx
@@ -1,8 +1,9 @@
 import Items from "./Items";
 import styles from "./toDostyle.module.css"
 export default function ToDoList({toDos}){
+    // console.log(toDos.length);
     return (
-        <div className={styles.list}>
+        <div className={toDos.length ? styles.list : ""}>
             {toDos.map((item)=>(<Items key={item} item={item}/>))}
         </div>
     )
