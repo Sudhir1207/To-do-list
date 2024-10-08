e03119adeff9730d3c141d21459aabf3cd886692
sudhirr@karunya.edu.in <sudhirr@karunya.edu.in>
2024-10-08 21:35:24 +0530

    Marking ToDo items as complete

--- to-do-app/src/components/Items.jsx
+++ to-do-app/src/components/Items.jsx
@@ -3,13 +3,20 @@ export default function Items({item, toDos, SettoDos }) {
     function handleDelete(item){
         console.log("item deleted: ", item); 
         SettoDos(toDos.filter((todo)=> todo !== item));
-        
-
     }
+
+    function handleClick(item){
+        const newArray = toDos.map((todo)=>todo.name === item.name ? {...todo, done: !todo.done}: todo);
+        SettoDos(newArray);
+        console.log(toDos);
+    }
+
+    const strike = item.done ? styles.completed : "";
     return (
         <div>
             <div className={styles.It_comp}>
-            <h3 className={styles.It_style}>{item.name}
+            <h3 className={styles.It_style}>
+            <span className={strike} onClick={()=>handleClick(item)}>{item.name}</span>
             <span>
                 <button onClick={()=>handleDelete(item)} className={styles.deleteButton}>Delete</button>
             </span>
