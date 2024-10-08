82813eff858aa54ded43bcec0f40f30807c626b8
sudhirr@karunya.edu.in <sudhirr@karunya.edu.in>
2024-10-08 23:08:37 +0530

    sorting based on completed tasks

--- to-do-app/src/components/ToDoList.jsx
+++ to-do-app/src/components/ToDoList.jsx
@@ -2,9 +2,10 @@ import Items from "./Items";
 import styles from "./toDostyle.module.css"
 export default function ToDoList({toDos, SettoDos}){
     // console.log(toDos.length);
+    const sortedTodos = toDos.slice().sort((a,b)=>Number(a.done)- Number(b.done));
     return (
         <div className={toDos.length ? styles.list : ""}>
-            {toDos.map((item)=>(
+            {sortedTodos.map((item)=>(
                 <Items key={item.name} item={item} toDos={toDos} SettoDos={SettoDos}/>
                 ))}
         </div>
